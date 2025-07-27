bc5fd9062124d1ee3075c0edcd8d060c80d149e8
linge-human <lingeshinice@outlook.com>
2025-07-27 15:45:52 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function unlockNotion() {
     let code = prompt("Enter Activation Code:");
-    if (code === "LOTR@linge") {
+    if (code === "helloworld") {
         document.getElementById("notionMessage").innerHTML = '<a href="https://www.notion.so/your-notion-link">Access Granted</a>';
     } else {
         document.getElementById("notionMessage").innerHTML = "Access Denied!";
