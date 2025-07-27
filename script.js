acf8af50a8ba0c66c76a8085b6635f9a1b00def2
linge-human <lingeshinice@outlook.com>
2025-07-27 16:00:48 +1000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function unlockNotion() {
-    let code = prompt("Enter Activation Code:");
-    if (code === "helloworld") {
-        document.getElementById("notionMessage").innerHTML = '<a href="https://www.notion.so/your-notion-link">Access Granted</a>';
-    } else {
-        document.getElementById("notionMessage").innerHTML = "Access Denied!";
-    }
-}
